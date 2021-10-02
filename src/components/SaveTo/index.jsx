ee7e526c214108d23ad63a528b48cb62cfd24550
nmhung1234 <nmhung1234@hotmail.com>
2021-10-02 10:52:27 +0700

    update ui

--- src/components/SaveTo/index.jsx
+++ src/components/SaveTo/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Button, TextInput, SelectField, option } from "evergreen-ui";
+import {
+    Button,
+    TextInput,
+    SelectField,
+    TextInputField,
+    toaster,
+} from "evergreen-ui";
 import { SaveToContainer } from "./styles";
 import { remote } from "electron";
 import { connect } from "react-redux";
@@ -7,43 +13,73 @@ import { SaveDirectotyAction } from "../../action";
 let dialog = remote.dialog;
 const ButtonSave = (props) => {
     const { saveDirectory } = props;
-    const [directoryState, SetDirectoryState] = React.useState("None Selected");
+    const [directoryState, setDirectoryState] = React.useState("None Selected");
+    const [nameGameState, setNameGameState] = React.useState("sky");
+    const [ideaGameState, setIdeaGameState] = React.useState("");
     const handleDirectory = async () => {
         const result = await dialog.showOpenDialog({
             properties: ["openDirectory"],
         });
         console.log("directories selected", result.filePaths);
         saveDirectory(result.filePaths);
-        SetDirectoryState((pre) => result.filePaths);
+        setDirectoryState((pre) => result.filePaths);
+    };
+    const handleNameGame = (e) => {
+        setNameGameState(e.target.value);
+    };
+    const handlIdeaGame = (e) => {
+        setIdeaGameState(e.target.value);
+    };
+    const handleExportAds = () => {
+        toaster.success("Export Ads Successfully!", {
+            id: "forbidden-action",
+        });
     };
     return (
         <SaveToContainer>
-            <SelectField
-                isInvalid
-                required
-                label="A required text input field"
-                description="Select Game to export"
-                validationMessage="This field is required"
-            >
-                <option value="foo" selected>
-                    Sky
-                </option>
-                <option value="bar">Bino1</option>
-                <option value="bar">Bino2</option>
-                <option value="bar">Beauty&Love</option>
-                <option value="bar">Juice</option>
-                <option value="bar">Paint</option>
-            </SelectField>
-            <div>
-                <TextInput disabled value={directoryState} />
-                <Button
-                    marginRight={16}
-                    appearance="primary"
-                    onClick={handleDirectory}
-                >
-                    Save to ...
-                </Button>
+            <div className="setup">
+                <div className="name-idea">
+                    <SelectField
+                        isInvalid
+                        required
+                        label="Choose Game"
+                        validationMessage="This field is required"
+                        onChange={handleNameGame}
+                        value={nameGameState}
+                    >
+                        <option value="sky">Sky</option>
+                        <option value="bino1">Bino1</option>
+                        <option value="bino2">Bino2</option>
+                        <option value="mano">Mano</option>
+                        <option value="paint">Paint</option>
+                        <option value="juice">Juice</option>
+                        <option value="bl">Beauty&Love</option>
+                    </SelectField>
+                    <TextInputField
+                        isInvalid
+                        required
+                        label="Idea for game"
+                        placeholder="Ex: Sky solo squad..."
+                        validationMessage="This field is required"
+                        onChange={handlIdeaGame}
+                        value={ideaGameState}
+                    />
+                </div>
+                <div>
+                    <TextInput disabled value={directoryState} />
+                    <Button marginLeft={16} onClick={handleDirectory}>
+                        Save to ...
+                    </Button>
+                </div>
             </div>
+            <Button
+                marginTop={50}
+                appearance="primary"
+                size="large"
+                onClick={handleExportAds}
+            >
+                Export Ads
+            </Button>
         </SaveToContainer>
     );
 };
--- src/components/SaveTo/styles.js
+++ src/components/SaveTo/styles.js
@@ -2,7 +2,17 @@ import styled from 'styled-components';
 
 export const SaveToContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: space-around;
-    align-items: center;
+    align-items: flex-start;
+    padding: 50px;
+    .setup{
+        display: flex;
+        flex-direction: column;
+        .name-idea{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+    }
 `
